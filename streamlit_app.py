75001533fe35de307773b97fa912fd4f96a2cbbb
databastard1 <124165432+databastard1@users.noreply.github.com>
2023-02-01 13:42:02 +0000

    Update streamlit_app.py
    added breakfast menu header
    added menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('I am a data bastard')
+
+
+streamlit.header('breakfast menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
