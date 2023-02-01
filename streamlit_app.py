800c0ed229087bf9183096c97c72d5553bcd0ba8
databastard1 <124165432+databastard1@users.noreply.github.com>
2023-02-01 13:44:41 +0000

    Update streamlit_app.py
    added emojis and changed names

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit
 
-streamlit.title('I am a data bastard')
+streamlit.title('Mom''s Spaghetti')
 
 
-streamlit.header('breakfast menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favourites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
