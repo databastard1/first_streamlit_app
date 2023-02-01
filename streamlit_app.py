c2db754cb46c5192d6f10135b1fa62e62e906791
databastard1 <124165432+databastard1@users.noreply.github.com>
2023-02-01 13:45:26 +0000

    Update streamlit_app.py
    added smoothie header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('Mom''s Spaghetti')
+streamlit.title('Moms Spaghetti')
 
 
 streamlit.header('Breakfast Favourites')
@@ -8,3 +8,4 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
